e860451df8483bd1c2f0a85c615b3b2d8ef7f21b
Evyats <evyats@gmail.com>
2026-01-08 17:21:40 +0200

    enhance db code and integration

--- config.py
+++ config.py
@@ -5,17 +5,15 @@ load_dotenv()
 
 class Settings:
     PORT: str
-    DB_IP: str
-    SOME_ENV_VAR: str
+    DATABASE_URL: str
 
 def getSettings():
     settings = Settings()
     settings.PORT = os.getenv("PORT", default="missing_env_var")
-    settings.DB_IP = os.getenv("DB_IP", default="missing_env_var")
-    settings.SOME_ENV_VAR = os.getenv("SOME_ENV_VAR", default="missing_env_var")
+    settings.DATABASE_URL = os.getenv("DATABASE_URL", default="missing_env_var")
     return settings
 
 if __name__ == "__main__":
     pass
     settings = getSettings()
-    print(settings.DB_IP)
+    print(settings.DATABASE_URL)
