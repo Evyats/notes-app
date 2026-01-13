d0723d4c3e64bad50318f4bdf4248494eccbfc5f
Evyats <evyats@gmail.com>
2026-01-13 13:05:44 +0200

    move sql command to separate files

---
+++ src/repositories/notes.py
@@ -0,0 +1,57 @@
+from .. import db
+
+
+def list_notes(user_id: int):
+    return db.execute_sql(
+        """
+        SELECT id, created FROM notes WHERE user_id=:user_id
+        """,
+        {"user_id": user_id},
+    )
+
+
+def create_note(user_id: int, note: str, created):
+    return db.execute_sql(
+        """
+        INSERT INTO notes
+        (user_id, note, created)
+        VALUES
+        (:user_id, :note, :created)
+        RETURNING id, user_id, created
+        """,
+        {"user_id": user_id, "note": note, "created": created},
+    )
+
+
+def get_note(user_id: int, note_id: int):
+    return db.execute_sql(
+        """
+        SELECT id, note, created
+        FROM notes
+        WHERE user_id=:user_id AND id=:note_id
+        """,
+        {"user_id": user_id, "note_id": note_id},
+    )
+
+
+def delete_note(note_id: int):
+    return db.execute_sql(
+        """
+        DELETE FROM notes
+        WHERE id=:note_id
+        RETURNING *
+        """,
+        {"note_id": note_id},
+    )
+
+
+def update_note(note_id: int, note: str):
+    return db.execute_sql(
+        """
+        UPDATE notes
+        SET note=:note
+        WHERE id=:note_id
+        RETURNING *
+        """,
+        {"note_id": note_id, "note": note},
+    )
---
+++ src/repositories/users.py
@@ -0,0 +1,85 @@
+from .. import db
+
+
+def get_user_by_email(email: str):
+    return db.execute_sql(
+        """
+        SELECT id, password_hash
+        FROM users
+        WHERE email=:email
+        """,
+        {"email": email},
+    )
+
+
+def create_user(email: str, password_hash: str, created):
+    return db.execute_sql(
+        """
+        INSERT INTO users
+        (email, password_hash, created)
+        VALUES
+        (:email, :password_hash, :created)
+        RETURNING id
+        """,
+        {"email": email, "password_hash": password_hash, "created": created},
+    )
+
+
+def list_users(offset: int, limit: int):
+    return db.execute_sql(
+        """
+        SELECT u.id, u.email, u.created, COUNT(n.note) AS notes_count
+        FROM users u
+        LEFT JOIN notes n ON u.id=n.user_id
+        GROUP BY u.id
+        ORDER BY u.created DESC
+        OFFSET :offset
+        LIMIT :limit
+        """,
+        {"offset": offset, "limit": limit},
+    )
+
+
+def get_user(user_id: int):
+    return db.execute_sql(
+        """
+        SELECT u.id, u.email, u.created, COUNT(n.note) AS notes_count
+        FROM users u
+        LEFT JOIN notes n ON u.id=n.user_id
+        WHERE u.id=:user_id
+        GROUP BY u.id
+        """,
+        {"user_id": user_id},
+    )
+
+
+def delete_user_notes(user_id: int):
+    return db.execute_sql(
+        """
+        DELETE FROM notes
+        WHERE user_id=:user_id
+        RETURNING *
+        """,
+        {"user_id": user_id},
+    )
+
+
+def delete_user(user_id: int):
+    return db.execute_sql(
+        """
+        DELETE FROM users
+        WHERE id=:user_id
+        RETURNING *
+        """,
+        {"user_id": user_id},
+    )
+
+
+def user_exists(user_id: int) -> bool:
+    rows = db.execute_sql(
+        """
+        SELECT id FROM users WHERE id=:user_id
+        """,
+        {"user_id": user_id},
+    )
+    return len(rows) > 0
